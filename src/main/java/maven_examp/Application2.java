78c4e8e0d273905c7019773925b87eff7f4a5f82
GIDDALURI-BALA-VENKATA-PRAVEEN <gbvp910@gmail.com>
2024-04-28 10:52:08 +0530

    initial commit for this project

---
+++ src/main/java/maven_examp/Application2.java
@@ -0,0 +1,10 @@
+package maven_examp;
+
+public class Application2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world");
+	}
+
+}
---
+++ target/generated-sources/archetype/src/main/resources/archetype-resources/src/main/java/Application2.java
@@ -0,0 +1,13 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+public class Application2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello world");
+	}
+
+}
